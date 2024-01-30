777f3a89d6e863b1acc5f45a40cc38ebc6a0c412
tuulikoo <tuuli.kivisaari@gmail.com>
2024-01-30 11:19:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# QRcode_test
+[Github](https://github.com/tuulikoo/QRcode_test)
+CLI-app for testing QRcode generation.
+
+Uses crates [qrcode v0.13.0](https://crates.io/crates/qrcode) for QR-code generation and [sha256](https://crates.io/crates/sha256) for hash-function.
+
+## Usage
+Generates a given string by default to the command line.
+- String is set by running with 
+```bash
+cargo run `<input string>`
+```
+- If no input string is given, a default string is used
+- The appearance of the generated QR code in the command line depends on whether a dark or light theme is used in the editor.
+
+Make sure to replace `<input string>` with the actual input string you want to use.
+
+![Alt text](<Screenshot 2024-01-30 at 11.10.39.png>)
+
+An image is also generated to folder img. Image name is your given input string hashed with sha256. Image can be opened by clicking and choosing "open with live server".
+
+![Alt text](<Screenshot 2024-01-30 at 11.10.50.png>)
+
+## Tests
+Run tests with the following commands: 
+
+```bash
+cargo test
+```
+
+or
+```bash
+cargo tarpaulin
+```
+
+No input string is required for running tests.
---
+++ src/main.rs
@@ -0,0 +1,164 @@
+use qrcode::QrCode;
+use qrcode::render::svg;
+use std::{env, sync::Mutex};
+use std::fs::File;
+use std::io::Write;
+use sha2::{Sha256, Digest};
+
+//Run main function with "cargo run <input string>"
+//If no input string given, default string used
+//Run tests with "cargo test" or "cargo tarpaulin" (no input string)
+#[cfg(not(tarpaulin_include))]
+fn main() {
+    //read from cli
+    let input_string = read_args();
+
+    // Encode input string into a QR code
+    let code = encode_qr_code(&input_string);
+
+    // Save the QR code as an SVG image and call cli-print function
+    save_svg_image(&code, &input_string);
+}
+
+fn encode_qr_code(input: &str) -> QrCode {
+    QrCode::new(input.as_bytes()).unwrap()
+}
+
+//Print function, buffer for testing
+fn print_qr_code(qr_code: &QrCode, buffer: &Mutex<Vec<u8>>) {
+    let printable = qr_code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+
+    print!("{}", printable);
+
+    // Ignore errors while writing to the buffer
+    let _ = buffer.lock().unwrap().write_all(printable.as_bytes());
+}
+
+
+fn save_svg_image(qr_code: &QrCode, input: &str) {
+    // Generate filename for SVG image with hash-function
+    let file_name = format!("img/{}.svg", hash(input));
+
+    // Render the QR code into an SVG image
+    let image = qr_code.render()
+        .min_dimensions(200, 200)
+        .dark_color(svg::Color("#000000"))  //Black for the QR code
+        .light_color(svg::Color("#FFFFFF")) // White for the background
+        .build();
+
+    // Save the SVG image to the hashed filename
+    let mut file = File::create(&file_name).expect("Unable to create file");
+    write!(file, "{}", image).expect("Unable to write to file");
+
+    // Print a message to show the input and the filename
+    println!("QR code image generated for '{}': {}", input, file_name);
+    println!("To view the QR code, open the generated SVG file using a live server.");
+    
+    //Print QR code to console
+    print_qr_code(&qr_code, &Mutex::new(Vec::new()));
+}
+
+fn read_args() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    // If at least one argument is provided, join all arguments into a single string
+    if args.len() >= 2 {
+        return args[1..].join(" ");
+    }
+    // If no arguments, return default string
+    #[cfg(not(tarpaulin_include))]
+    "--test-threads 1".to_string()
+}
+
+fn hash(word: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(word.as_bytes());
+    let result = hasher.finalize();
+
+    result
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+    use crate::{encode_qr_code, print_qr_code, save_svg_image};
+    use super::{hash, read_args};
+    use std::fs;
+
+    #[test]
+    fn test_hash() {
+        let word = String::from("Hello world");
+        let result: String = hash(&word);
+        assert_eq!(
+            result,
+            "64ec88ca00b268e5ba1a35678a1b5316d212f4f366b2477232534a8aeca37f3c"
+        );
+    }
+
+    #[test]
+    fn test_empty_hash() {
+        let word: String = String::from("");
+        let result: String = hash(&word);
+        assert_eq!(
+            result,
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        );
+    }
+    
+
+    #[test]
+    fn test_read_args() {
+        let word = read_args();
+        assert_eq!(word, "--test-threads 1");
+    }
+
+    #[test]
+    fn test_save_svg_image() {
+        let input = "test_input";
+        let qr_code = encode_qr_code(input);
+
+        save_svg_image(&qr_code, input);
+
+        // Verify file is created
+        let expected_file_name = format!("img/{}.svg", hash(input));
+        assert!(fs::metadata(&expected_file_name).is_ok());
+    }
+
+    #[test]
+    fn test_print_qr_code_success() {
+        let input = "test_input";
+        let qr_code = encode_qr_code(input);
+
+        // Create a Mutex to synchronize access to the buffer
+        let buffer = Mutex::new(Vec::new());
+
+        // Print QR code to buffer
+        print_qr_code(&qr_code, &buffer);
+
+        // Access buffer and check contents
+        let content = buffer.lock().unwrap();
+
+        let expected_printable = qr_code
+            .render::<char>()
+            .quiet_zone(false)
+            .module_dimensions(2, 1)
+            .build();
+
+        // Convert the expected printable to bytes
+        let expected_bytes = expected_printable.as_bytes();
+
+        // Assert that the contents of the buffer match the expected printable representation
+        assert_eq!(content.as_slice(), expected_bytes);
+    }
+
+    
+}
+
